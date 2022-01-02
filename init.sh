0ccb09e792d24986e3bfb19a239e0a0e64a7f867
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-01-02 17:42:41 -0500

    add initalization script

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/storywriter/
 git pull
+pip3 install -r requirements.txt
 if [ ! -d ~/storywriter/hg_models/ ]; then
     gsutil -m cp -r  gs://ks-story-ew4-storage/hg_models/ .
 fi
